9431553703b0bb3e40fd92918a40a043ab52de65
PedroHamamoto <pedrohamamoto@hotmail.com>
2016-06-20 22:35:52 -0300

    Project setup and a hello-world endpoint

---
+++ src/main/java/br/com/challenge2/ApplicationConfig.java
@@ -0,0 +1,16 @@
+package br.com.challenge2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Class responsible for configure the application
+ */
+@SpringBootApplication
+public class ApplicationConfig {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationConfig.class, args);
+    }
+
+}
